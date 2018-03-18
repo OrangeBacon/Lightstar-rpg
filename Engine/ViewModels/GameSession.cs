1ca270dbff75091a647c9f60cb42800b04886f67
ScratchOs <Computer.backup.15@gmail.com>
2018-03-18 18:08:58 +0000

    UI Update

---
+++ Engine/ViewModels/GameSession.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Models;
+
+namespace Engine.ViewModels {
+    public class GameSession {
+        public Player CurrentPlayer { get; set; }
+
+        public GameSession() {
+            CurrentPlayer = new Player() {
+                Name = "Alanis",
+                CharacterClass = "Fighter",
+                HitPoints = 10,
+                Gold = 100000,
+                ExperiencePoints = 0,
+                Level = 1
+            };
+        }
+    }
+}
--- UserInterface/MainWindow.xaml.cs
+++ UserInterface/MainWindow.xaml.cs
@@ -12,14 +12,24 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Engine.ViewModels;
 
 namespace UserInterface {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
+        private GameSession _gameSession;
         public MainWindow() {
             InitializeComponent();
+
+            _gameSession = new GameSession();
+
+            DataContext = _gameSession;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            _gameSession.CurrentPlayer.ExperiencePoints += 10;
         }
     }
 }
